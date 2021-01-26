2c103d3a463483f87b3b8adf70b520b68e83c047
Joseph Montesano <jmmmontesano@gmail.com>
2021-01-26 09:42:12 -0500

    updated geneholder

--- src/simulationComponents/geneHolder/geneHolder.js
+++ src/simulationComponents/geneHolder/geneHolder.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import './geneHolder.css';
 import { Link } from 'react-router-dom';
+import Collapsible from 'react-collapsible';
 
 class GeneHolder extends Component {
   render() {
@@ -9,119 +10,57 @@ class GeneHolder extends Component {
     if (this.props.location) {
       geneCategories = (
         <div className='geneCategory'>
+       
           <section className='geneInformation SBG'>
-            <div className='geneTile'>
-              {' '}
+            {/* <div className="geneTitle">{this.props.location.state.species} Genes</div> */}
+          
+          <div className='geneTile'>
               <Link
                 to={{
-                  pathname: `/simulation/genes/${this.props.location.state.species}/nGRi`,
-                  key: 'nGRi',
+                  pathname: `/simulation/genes/${this.props.location.state.species}/LS`,
+                  key: 'LS',
                   state: {
-                    name: 'nGRi',
-                    listOfGenes: 'ABab',
+                    name: 'LS',
+                    listOfGenes: 'NPOnpo',
                     speciesNumber: this.props.location.state.speciesNumber,
                     expressionValues: this.props.location.state
                       .expressionValues,
-                    A: Math.round(
-                      (this.props.location.state.currentValues.A /
+                    N: Math.round(
+                      (this.props.location.state.currentValues.N /
                         this.props.location.state.currentValues.total) *
                         100
                     ),
-                    a: Math.round(
-                      (this.props.location.state.currentValues.a /
+                    O: Math.round(
+                      (this.props.location.state.currentValues.O /
                         this.props.location.state.currentValues.total) *
                         100
                     ),
-                    B: Math.round(
-                      (this.props.location.state.currentValues.B /
+                    P: Math.round(
+                      (this.props.location.state.currentValues.P /
                         this.props.location.state.currentValues.total) *
                         100
                     ),
-                    b: Math.round(
-                      (this.props.location.state.currentValues.b /
+                    n: Math.round(
+                      (this.props.location.state.currentValues.n /
                         this.props.location.state.currentValues.total) *
                         100
                     ),
-                  },
-                }}
-              >
-                nGRi
-              </Link>
-            </div>
-            <div className='geneTile'>
-              {' '}
-              <Link
-                to={{
-                  pathname: `/simulation/genes/${this.props.location.state.species}/pGRi`,
-                  key: 'pGRi',
-                  state: {
-                    name: 'pGRi',
 
-                    listOfGenes: 'CDcd',
-                    speciesNumber: this.props.location.state.speciesNumber,
-                    expressionValues: this.props.location.state
-                      .expressionValues,
-                    C: Math.round(
-                      (this.props.location.state.currentValues.C /
+                    o: Math.round(
+                      (this.props.location.state.currentValues.o /
                         this.props.location.state.currentValues.total) *
                         100
                     ),
-                    c: Math.round(
-                      (this.props.location.state.currentValues.c /
-                        this.props.location.state.currentValues.total) *
-                        100
-                    ),
-                    D: Math.round(
-                      (this.props.location.state.currentValues.D /
-                        this.props.location.state.currentValues.total) *
-                        100
-                    ),
-                    d: Math.round(
-                      (this.props.location.state.currentValues.d /
+
+                    p: Math.round(
+                      (this.props.location.state.currentValues.p /
                         this.props.location.state.currentValues.total) *
                         100
                     ),
                   },
                 }}
               >
-                pGRi
-              </Link>
-            </div>
-            <div className='geneTile'>
-              <Link
-                to={{
-                  pathname: `/simulation/genes/${this.props.location.state.species}/pBi`,
-                  key: 'pBi',
-                  state: {
-                    name: 'pBi',
-                    listOfGenes: 'EFef',
-                    speciesNumber: this.props.location.state.speciesNumber,
-                    expressionValues: this.props.location.state
-                      .expressionValues,
-                    E: Math.round(
-                      (this.props.location.state.currentValues.E /
-                        this.props.location.state.currentValues.total) *
-                        100
-                    ),
-                    e: Math.round(
-                      (this.props.location.state.currentValues.e /
-                        this.props.location.state.currentValues.total) *
-                        100
-                    ),
-                    F: Math.round(
-                      (this.props.location.state.currentValues.F /
-                        this.props.location.state.currentValues.total) *
-                        100
-                    ),
-                    f: Math.round(
-                      (this.props.location.state.currentValues.f /
-                        this.props.location.state.currentValues.total) *
-                        100
-                    ),
-                  },
-                }}
-              >
-                pBi
+                Life Span
               </Link>
             </div>
             <div className='geneTile'>
@@ -168,58 +107,126 @@ class GeneHolder extends Component {
                   },
                 }}
               >
-                GR
+                Growth Rate
               </Link>
             </div>
+            <Collapsible trigger="Additional Traits">
             <div className='geneTile'>
+              {' '}
               <Link
                 to={{
-                  pathname: `/simulation/genes/${this.props.location.state.species}/LS`,
-                  key: 'LS',
+                  pathname: `/simulation/genes/${this.props.location.state.species}/nGRi`,
+                  key: 'nGRi',
                   state: {
-                    name: 'LS',
-                    listOfGenes: 'NPOnpo',
+                    name: 'nGRi',
+                    listOfGenes: 'ABab',
                     speciesNumber: this.props.location.state.speciesNumber,
                     expressionValues: this.props.location.state
                       .expressionValues,
-                    N: Math.round(
-                      (this.props.location.state.currentValues.N /
+                    A: Math.round(
+                      (this.props.location.state.currentValues.A /
                         this.props.location.state.currentValues.total) *
                         100
                     ),
-                    O: Math.round(
-                      (this.props.location.state.currentValues.O /
+                    a: Math.round(
+                      (this.props.location.state.currentValues.a /
                         this.props.location.state.currentValues.total) *
                         100
                     ),
-                    P: Math.round(
-                      (this.props.location.state.currentValues.P /
+                    B: Math.round(
+                      (this.props.location.state.currentValues.B /
                         this.props.location.state.currentValues.total) *
                         100
                     ),
-                    n: Math.round(
-                      (this.props.location.state.currentValues.n /
-                        this.props.location.state.currentValues.total) *
-                        100
-                    ),
-
-                    o: Math.round(
-                      (this.props.location.state.currentValues.o /
-                        this.props.location.state.currentValues.total) *
-                        100
-                    ),
-
-                    p: Math.round(
-                      (this.props.location.state.currentValues.p /
+                    b: Math.round(
+                      (this.props.location.state.currentValues.b /
                         this.props.location.state.currentValues.total) *
                         100
                     ),
                   },
                 }}
               >
-                LS
+                Negative Growth Rate Influence
               </Link>
             </div>
+            <div className='geneTile'>
+              {' '}
+              <Link
+                to={{
+                  pathname: `/simulation/genes/${this.props.location.state.species}/pGRi`,
+                  key: 'pGRi',
+                  state: {
+                    name: 'pGRi',
+
+                    listOfGenes: 'CDcd',
+                    speciesNumber: this.props.location.state.speciesNumber,
+                    expressionValues: this.props.location.state
+                      .expressionValues,
+                    C: Math.round(
+                      (this.props.location.state.currentValues.C /
+                        this.props.location.state.currentValues.total) *
+                        100
+                    ),
+                    c: Math.round(
+                      (this.props.location.state.currentValues.c /
+                        this.props.location.state.currentValues.total) *
+                        100
+                    ),
+                    D: Math.round(
+                      (this.props.location.state.currentValues.D /
+                        this.props.location.state.currentValues.total) *
+                        100
+                    ),
+                    d: Math.round(
+                      (this.props.location.state.currentValues.d /
+                        this.props.location.state.currentValues.total) *
+                        100
+                    ),
+                  },
+                }}
+              >
+                Positive Growth Rate Influence
+              </Link>
+            </div>
+            <div className='geneTile'>
+              <Link
+                to={{
+                  pathname: `/simulation/genes/${this.props.location.state.species}/pBi`,
+                  key: 'pBi',
+                  state: {
+                    name: 'pBi',
+                    listOfGenes: 'EFef',
+                    speciesNumber: this.props.location.state.speciesNumber,
+                    expressionValues: this.props.location.state
+                      .expressionValues,
+                    E: Math.round(
+                      (this.props.location.state.currentValues.E /
+                        this.props.location.state.currentValues.total) *
+                        100
+                    ),
+                    e: Math.round(
+                      (this.props.location.state.currentValues.e /
+                        this.props.location.state.currentValues.total) *
+                        100
+                    ),
+                    F: Math.round(
+                      (this.props.location.state.currentValues.F /
+                        this.props.location.state.currentValues.total) *
+                        100
+                    ),
+                    f: Math.round(
+                      (this.props.location.state.currentValues.f /
+                        this.props.location.state.currentValues.total) *
+                        100
+                    ),
+                  },
+                }}
+              >
+                Positive Breeding Influence
+              </Link>
+            </div>
+            </Collapsible>
+
           </section>
         </div>
       );
